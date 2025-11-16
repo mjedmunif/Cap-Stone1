e094bc9a2cc87574f4e8c9cab239e4443441b6da
M <m@MacBook-Air-mjed.local>
2025-11-16 17:45:31 +0300

    CRUD for Product

--- src/main/java/org/example/capstone1/Controller/ProductController.java
+++ src/main/java/org/example/capstone1/Controller/ProductController.java
@@ -1,4 +1,61 @@
 package org.example.capstone1.Controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.capstone1.Api.ApiResponse;
+import org.example.capstone1.Model.Product;
+import org.example.capstone1.Service.ProductSystem;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductSystem productSystem;
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getProducts(){
+        if (productSystem.getProducts().isEmpty()){
+            return ResponseEntity.status(404).body(new ApiResponse("there is no products"));
+        }
+        return ResponseEntity.status(200).body(productSystem.getProducts());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addNewProducts(@RequestBody @Valid Product product , Errors errors){
+        if (errors.hasErrors()){
+            String message = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(message);
+        }
+        boolean checked = productSystem.addProduct(product);
+        if (checked){
+            return ResponseEntity.status(200).body(new ApiResponse("added product successfully"));
+        }
+        return ResponseEntity.status(404).body(new ApiResponse("invalid category id"));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable String id , @RequestBody @Valid Product product , Errors errors){
+        if (errors.hasErrors()){
+            String message = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(message);
+        }
+        boolean isUpdated = productSystem.updateProduct(id, product);
+        if (isUpdated){
+            return ResponseEntity.status(200).body(new ApiResponse("updated successfully"));
+        }
+        return ResponseEntity.status(404).body(new ApiResponse("Not found"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable String id){
+        boolean deleted = productSystem.deleteProduct(id);
+        if (deleted){
+            return ResponseEntity.status(200).body(new ApiResponse("deleted successfully"));
+        }
+        return ResponseEntity.status(404).body(new ApiResponse("updated successfully"));
+    }
 }
--- src/main/java/org/example/capstone1/Service/ProductSystem.java
+++ src/main/java/org/example/capstone1/Service/ProductSystem.java
@@ -1,5 +1,53 @@
 package org.example.capstone1.Service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.example.capstone1.Model.Category;
+import org.example.capstone1.Model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class ProductSystem {
-    
+
+    private final CategorySystem categorySystem;
+
+    @Getter
+    ArrayList<Product> products = new ArrayList<>();
+
+    public ProductSystem(CategorySystem categorySystem) {
+        this.categorySystem = categorySystem;
+    }
+
+    public boolean addProduct(Product product){
+        for (Category c : categorySystem.categories){
+           if (c.getId().equalsIgnoreCase(product.getCategoryId())){
+               products.add(product);
+               return true;
+           }
+        }
+        return false;
+    }
+
+
+    public boolean updateProduct(String id , Product product){
+        for (int i = 0 ; i < products.size() ; i++){
+            if (products.get(i).getId().equalsIgnoreCase(id)){
+                products.set(i , product);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteProduct(String id){
+        for (int i = 0 ; i < products.size() ; i++){
+            if (products.get(i).getId().equalsIgnoreCase(id)){
+                products.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
